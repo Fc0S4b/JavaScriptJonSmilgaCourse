cd9b79c39280f364c6cd95745216856bc80111e3
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-03 23:45:25 -0400

    menu terminado

--- 04-DOM-projects/08-menu/setup/app.js
+++ 04-DOM-projects/08-menu/setup/app.js
@@ -71,13 +71,54 @@ const menu = [
     img: './images/item-9.jpeg',
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+
+  {
+    id: 10,
+    title: 'steak dinner',
+    category: 'dinner',
+    price: 39.99,
+    img: './images/item-10.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const container = document.querySelector('.btn-container');
+// const filterBtns = document.querySelectorAll('.filter-btn');
+// console.log(filterBtns); //NodeList is empty (despúes de activar los botones dinámicos), se arregla dejando filterBtns dentro de categories
 
+// load items
 window.addEventListener('DOMContentLoaded', function () {
   // console.log('shake and bake');
-  let displayMenu = menu.map(function (item) {
+  displayMenuItems(menu);
+  // const categories = menu.map(function (item) {
+  //   return item.category;
+  // });
+  // console.log(categories); se ven todas las categorías en consolas pero no están reducidas a categorías no repetidas
+  displayMenuBtns();
+});
+
+// refactorizando filter dentro de categories
+// // filter items
+// filterBtns.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//     // console.log(e.currentTarget.dataset.id)
+//     const category = e.currentTarget.dataset.id;
+//     const menuCategory = menu.filter(function (menuItem) {
+//       // console.log(menuItem.category);
+//       if (menuItem.category === category) {
+//         return menuItem;
+//       }
+//     });
+//     if (category === 'all') {
+//       displayMenuItems(menu);
+//     } else {
+//       displayMenuItems(menuCategory);
+//     }
+//   });
+// });
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
     // console.log(item);
 
     // return item;
@@ -99,4 +140,49 @@ window.addEventListener('DOMContentLoaded', function () {
   displayMenu = displayMenu.join(''); //si no agregas "" se agregarán comas entre las etiquetas en el html insertado
   // console.log(displayMenu);
   sectionCenter.innerHTML = displayMenu;
-});
+}
+
+// Para agregar botones de categoría de forma dinámica
+// el archivo naive-txt tiene un "pantallazo" de lo que era el código antes de hacer esta modificación
+// get only unique categories - HARDEST ONE
+// iterate over categories return buttons
+// make sure to select buttons when they are available
+
+function displayMenuBtns() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ['all']
+  ); //reduce toma values con valor inicial en ['all'], por cada item que recorre en menu y si este no está en values, se agrega al array, creando un array con valores únicos, cuando ya no encuentre más valores repetidos, retornará el array completo
+  // console.log(categories);
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+    })
+    .join('');
+  // console.log(categoryBtns);
+  container.innerHTML = categoryBtns;
+  const filterBtns = container.querySelectorAll('.filter-btn'); //cambié a container ya que ahora se accede desde container y no document, aunque el resultado es el mismo es para mejorar la práctica para una selección más específica
+  // filter items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      // console.log(e.currentTarget.dataset.id)
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === 'all') {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
+}
