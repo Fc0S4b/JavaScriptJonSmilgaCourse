a7602626802e0c489fc734343757118c3c4b8aad
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-10 17:03:05 -0400

    dark mode ok

--- 04-DOM-projects/19-dark-mode/starter/app.js
+++ 04-DOM-projects/19-dark-mode/starter/app.js
@@ -1,1 +1,29 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
+
+toggleBtn.addEventListener('click', () => {
+  // console.log('hello');
+  document.documentElement.classList.toggle('dark-theme');
+});
+
+const articlesData = articles
+  .map((article) => {
+    //  console.log(article);
+    const { title, date, length, snippet } = article;
+    // format date
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+    return `<article class="post">
+      <h2>${title}</h2>
+      <div class="post-info">
+        <span>${formatDate}
+          <span>${length}</span>
+        </span>
+      </div>
+      <p>${snippet}</p>
+    </article>`;
+  })
+  .join('');
+
+// console.log(articles)
+
+articlesContainer.innerHTML = articlesData;
