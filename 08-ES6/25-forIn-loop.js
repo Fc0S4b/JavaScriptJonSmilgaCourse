d2bf33c7f542cf98f3b2c4781f548812c97d49a6
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-10 01:01:06 -0400

    for in listo

---
+++ 08-ES6/25-forIn-loop.js
@@ -0,0 +1,16 @@
+// "for in" loop - iterate over object properties
+// not adviced to use it on arrays, especially if the order is important
+// on arrays use "for of " loop instead
+
+const person = {
+  name: 'john',
+  age: 25,
+  status: 'student',
+};
+
+for (const propertyName in person) {
+  console.log(`${propertyName} : ${person[propertyName]}`);
+}
+// name: john;
+// age: 25;
+// status: student;
