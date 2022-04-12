904ea152f5c6c7ab88a2f63ed9d7fc938eed9fff
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-12 18:05:32 -0400

    AJAX http API done

---
+++ 12-AJAX/01-AJAX-http-API.js
@@ -0,0 +1,5 @@
+// APi https://www.freecodecamp.org/news/what-is-an-api-in-english-please-b880a3214a82/
+
+// AJAX https://www.w3schools.com/xml/ajax_intro.asp
+
+// HTTP Request Methods https://www.w3schools.com/tags/ref_httpmethods.asp
