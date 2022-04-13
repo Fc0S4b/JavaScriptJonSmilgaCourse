f3c54c3aef23e39289d24f4b9199a44186b060ce
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-13 17:38:50 -0400

    dad jokes done

--- 04-DOM-projects/21-dad-jokes/starter/app.js
+++ 04-DOM-projects/21-dad-jokes/starter/app.js
@@ -1,1 +1,37 @@
-console.log('Dad Jokes Starter');
+throw new Error('there was an error');
+
+const url = 'https://icanhazdadjoke.com/';
+
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+// console.log(btn, result)
+
+btn.addEventListener('click', () => {
+  // console.log('hello');
+  fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+  result.textContent = 'Loading...';
+  try {
+    const response = await fetch(url, {
+      // doc de esta api piden headers
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'learning app',
+      },
+    });
+    // console.log(response)
+    if (!response.ok) {
+      console.log('there was an error');
+    }
+    const data = await response.json();
+    // console.log(data)
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = 'There was an error...';
+  }
+};
+
+fetchDadJoke();
